20db5550c0da1fddf9d2468c135703344b39465f
changholee2010 <148027761+changholee2010@users.noreply.github.com>
2025-04-28 09:07:05 +0900

    Update condition6exe.js

--- js/condition6exe.js
+++ js/condition6exe.js
@@ -1,6 +1,15 @@
 // condition6exe.js
 // 1. 2개의 수를 입력. 큰값을 콘솔에 출력. "두수중에서 큰값은 28입니다."
 
+let num1, num2;
+
+num1 = parseInt(prompt("수를 입력: "));
+num2 = parseInt(prompt("수를 입력: "));
+if (num1 > num2) {
+  console.log("두수중에서 큰값은 " + num1 + "입니다.")
+} else {
+  console.log("두수중에서 큰값은 " + num2 + "입니다.")
+}
 // 2. 임의의 2개의 수(40~ 70)를 생성. "2개의 수중에서 큰값은 ??, 작은값은 ??"
 
 // 3. condition3.js에 "A","B","C","D","F" => 95이상이면 A+
