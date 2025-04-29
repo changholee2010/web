ea2e1890d719e037b64e9689c15f68693e384f1b
note <note@co.com>
2025-04-29 09:03:00 +0900

    for

--- js/function3exe.js
+++ js/function3exe.js
@@ -1,6 +1,30 @@
 // function3exe.js
 // 1. max(10, 17) => "2개의 숫자중에서 큰값은 17입니다."
-
+function max(num1, num2) {
+    if (num1 > num2) {
+        console.log(`2개의 숫자중에서 큰값은 ${num1}입니다.`)
+    } else {
+        console.log(`2개의 숫자중에서 큰값은 ${num2}입니다.`)
+    }
+}
 // 2. sumUpTo(5) => "1부터 5까지의 합은 15입니다."
+function sumUpTo(num1) {
+    let sum = 0;
+    for (let i = 1; i <= num1; i++) {
+        sum += i;
+    }
+    console.log(`1부터 ${num1}까지의 합은 ${sum}입니다.`)
+}
 
 // 3. diff(17, 34) => "두수의 차는 17입니다."
+function diff(num1, num2) {
+    let result = 0;
+    if (num1 > num2) {
+        result = num1 - num2;
+    } else {
+        result = num2 - num1;
+    }
+    console.log(`두수의 차는 ${result}입니다.`)
+}
+
+max(10, 4)
